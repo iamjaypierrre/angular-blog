f5d7db62674c08e2e76da8dd941200a750178b7a
Jean Pierre <jepiierre@gmail.com>
2022-12-15 13:59:40 -0300

    deixando a content page dinamica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,22 @@
 export const dataFake = [
     {
-        "id":1,
-        "photo":"link",
-        "title":"TITULO",
-        "description": "ola"
-    }
+        "id":"1",
+        "title":"ARGENTINA ENFRENTA CROACIA NA SEMINAL DA COPA DO MUNDO",
+        "description": "Argentinos passam das quartas de finais apos jogo eletrizante contra a holanda e garantem vaga na semifinal da copa.",
+        "photoCover":"https://static.poder360.com.br/2022/12/selecao-argentina-9.dez_-848x477.jpeg"
+    },
+
+    {
+        "id":"2",
+        "title":"ANCELOTTI NA SELECAO?",
+        "description": "CBF vai em busca do treinador italizano para comandar a amarelinha.",
+        "photoCover":"https://tntsports.com.br/__export/1670848052735/sites/esporteinterativo/img/2022/12/12/ancelotti_1.jpg"
+    },
+
+    {
+        "id":"3",
+        "title":"ANCELOTTI NA SELECAO?",
+        "description": "CBF vai em busca do treinador italizano para comandar a amarelinha.",
+        "photoCover":"https://tntsports.com.br/__export/1670848052735/sites/esporteinterativo/img/2022/12/12/ancelotti_1.jpg"
+    },
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -13,10 +15,27 @@ export class ContentComponent implements OnInit  {
   contentTitle:string="MINHA NOTICIA"
   @Input()
   contentDescription:string="LASJDLAKSJDL"
+  private id:string | null ="0"
 
-  constructor(){}
+  constructor(
+    private route:ActivatedRoute
+  ){}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = (value.get("id"))
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id.toString() == id)[0]
+
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
 
   }
 
